1387776f106541f3bfb7b37c196fde97f75a0421
Ubuntu <shady831213>
2018-10-29 09:18:44 -0700

    use nachos 2013; cross compiler ready

---
+++ nachos-java/nachos/bin/gcc-mips/lib/gcc/mipsel-elf/4.8.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --target=mipsel-elf --prefix=/home/yli/work/java/nachos/nachos-java/nachos/bin/gcc-mips --program-prefix=mips- --with-gnu-as --with-gnu-ld --enable-languages=c --with-gmp=/home/yli/work/java/nachos/nachos-java/nachos/bin/gcc-mips-dep --with-mpfr=/home/yli/work/java/nachos/nachos-java/nachos/bin/gcc-mips-dep --with-mpc=/home/yli/work/java/nachos/nachos-java/nachos/bin/gcc-mips-dep";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
