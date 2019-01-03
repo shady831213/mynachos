b659de433a55cb372d8356739119853db23acb9c
Ubuntu <shady831213>
2019-01-02 18:07:34 -0800

    add test

--- nachos-java/nachos/network/NetKernel.java
+++ nachos-java/nachos/network/NetKernel.java
@@ -36,79 +36,75 @@ public class NetKernel extends VMKernel {
      */
     public void selfTest() {
         super.selfTest();
+        Lib.TestSuite ts = new Lib.TestSuite();
 
-        KThread serverThread = new KThread(new Runnable() {
-            public void run() {
-                pingServer();
+        //basic socket test
+        ts.addTest(new Lib.Test("basic_socket_test", new Runnable() {
+            private void ping(int dstPort) {
+                int srcLink = Machine.networkLink().getLinkAddress();
+
+                Socket socket = new Socket(postOffice);
+                OpenFile file = socket.connect(srcLink, dstPort);
+                byte[] tdata, rdata;
+                tdata = new byte[456];
+                for (int i = 0; i < tdata.length; i++) {
+                    tdata[i] = (byte) (i % 255);
+                }
+                rdata = new byte[777];
+
+                file.write(tdata, 0, tdata.length);
+                file.close();
+                int rcnt = 0;
+                while (rcnt != rdata.length) {
+                    rcnt += file.read(rdata, rcnt, rdata.length);
+                }
+
+                for (int i = 0; i < rdata.length; i++) {
+                    Lib.assertTrue(rdata[i] == (byte) (255 - (i % 255)), "Client data " + i + " expect=" + (byte) (255 - (i % 255)) + " but actaul=" + rdata[i]);
+                }
             }
-        });
 
-        serverThread.fork();
+            private void pingServer() {
+                Socket socket = new Socket(postOffice);
+                OpenFile file = socket.accept(0);
+                byte[] tdata, rdata;
+                rdata = new byte[456];
+                int rcnt = 0;
+                while (rcnt != rdata.length) {
+                    rcnt += file.read(rdata, rcnt, rdata.length);
+                }
+                for (int i = 0; i < rdata.length; i++) {
+                    Lib.assertTrue(rdata[i] == (byte) (i % 255), "Server data " + i + " expect=" + (byte) (255 - (i % 255)) + " but actaul=" + rdata[i]);
+                }
 
-        System.out.println("Press any key to start the network test...");
-        console.readByte(true);
+                tdata = new byte[777];
+                for (int i = 0; i < tdata.length; i++) {
+                    tdata[i] = (byte) (255 - (i % 255));
+                }
+                file.write(tdata, 0, tdata.length);
+                file.close();
+            }
 
-        // ping this machine first
-        ping(0);
+            @Override
+            public void run() {
+                KThread serverThread = new KThread(new Runnable() {
+                    public void run() {
+                        pingServer();
+                    }
+                });
 
-        // if we're 0 or 1, ping the opposite
-//        if (local <= 1)
-//            ping(1 - local);
+                serverThread.fork();
+
+                console.readByte(true);
+
+                // ping this machine first
+                ping(0);
+            }
+        }));
+        ts.run();
+
     }
 
-    private void ping(int dstPort) {
-        int srcLink = Machine.networkLink().getLinkAddress();
-
-
-        long startTime = Machine.timer().getTime();
-        Socket socket = new Socket(postOffice);
-        OpenFile file = socket.connect(srcLink, dstPort);
-        System.out.println("PING port " + dstPort + " from " + socket.srcPort);
-        byte[] tdata, rdata;
-        tdata = new byte[3456];
-        for (int i = 0; i < tdata.length; i++) {
-            tdata[i] = (byte) (i % 255);
-        }
-        rdata = new byte[777];
-
-        file.write(tdata, 0, tdata.length);
-        file.close();
-        int rcnt = 0;
-        while (rcnt != rdata.length) {
-            rcnt += file.read(rdata, rcnt, rdata.length);
-        }
-
-        long endTime = Machine.timer().getTime();
-
-        System.out.println("time=" + (endTime - startTime) + " ticks");
-        System.out.println("Client get data :");
-        for (int i = 0; i < rdata.length; i++) {
-            System.out.println("Client rdata " + i + " = " + rdata[i]);
-        }
-    }
-
-    private void pingServer() {
-        Socket socket = new Socket(postOffice);
-        OpenFile file = socket.accept(0);
-        System.out.println("accept @ port " + 0 + " from " + socket.dstPort);
-        byte[] tdata, rdata;
-        rdata = new byte[3456];
-        int rcnt = 0;
-        while (rcnt != rdata.length) {
-            rcnt += file.read(rdata, rcnt, rdata.length);
-        }
-        System.out.println("Server get data :");
-        for (int i = 0; i < rdata.length; i++) {
-            System.out.println("Server rdata " + i + " = " + rdata[i]);
-        }
-
-        tdata = new byte[777];
-        for (int i = 0; i < tdata.length; i++) {
-            tdata[i] = (byte) (255 - (i % 255));
-        }
-        file.write(tdata, 0, tdata.length);
-        file.close();
-    }
 
     /**
      * Start running user programs.
